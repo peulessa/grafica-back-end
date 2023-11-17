d4b17a7f8e55e524a0ed7f7c338b06caaf2d8458
Pedro <pepeulessa@hotmail.com>
2023-11-17 10:40:12 -0300

    Ajustando DTO de criação de User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 0,
+        minUppercase: 0,
+        minNumbers: 0,
+        minSymbols: 0,
+    })
     password: string;
 }
