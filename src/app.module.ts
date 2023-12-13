bdd8d702aa808b936466f6483aa89ef8a211f2c6
Pedro <pepeulessa@hotmail.com>
2023-12-13 12:42:17 -0300

    Criando estrutura inicial da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as ConfigEnv from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigEnv.ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const config = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Modelo Back-end')
+    .setDescription(
+      'API utilizada como modelo para desenvolvimento do back-end',
+    )
+    .setVersion('1.0')
+    .addTag('Módulos')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const HTTP_PORT = config.get<string>('HTTP_PORT');
+  await app.listen(HTTP_PORT);
 }
 bootstrap();
