e2d17cff70cf27ae97e81423e08e52fbb41bc4b9
Pedro <pepeulessa@hotmail.com>
2023-12-13 12:53:36 -0300

    Criação dos módulos das tabelas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { FornecedoresModule } from './modules/fornecedores/fornecedores.module';
+import { VendasModule } from './modules/vendas/vendas.module';
+import { ComprasModule } from './modules/compras/compras.module';
 import * as ConfigEnv from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigEnv.ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    UsuariosModule,
+    ClientesModule,
+    FornecedoresModule,
+    VendasModule,
+    ComprasModule,
   ],
   controllers: [],
   providers: [],
