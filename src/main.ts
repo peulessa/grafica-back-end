3408ac004efb7b86bb5ef29e0a56287bddf01097
Pedro <pepeulessa@hotmail.com>
2023-12-14 13:16:50 -0300

    Alterando booststrap

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,19 +13,6 @@ async function bootstrap() {
     }),
   );
   const config = app.get(ConfigService);
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Modelo Back-end')
-    .setDescription(
-      'API utilizada como modelo para desenvolvimento do back-end',
-    )
-    .setVersion('1.0')
-    .addTag('Módulos')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
 
   const HTTP_PORT = config.get<string>('HTTP_PORT');
   await app.listen(HTTP_PORT);
